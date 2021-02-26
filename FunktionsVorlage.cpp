eaeb2092dbd62f2e82b4aad7d480d9d04ed5e640
Georg <48410861+weltbester@users.noreply.github.com>
2021-02-26 23:17:01 +0100

    Initial commit

---
+++ FunktionsVorlage.cpp
@@ -0,0 +1,45 @@
+/** 
+ * File: filename.cpp
+ * Author: Georg Pohl
+ * Date of creation: 1st of January 2021
+ * Version: 1.0
+ * Last change: 1st of January 2021
+ * -------------------------------------
+ * Description:
+ *  Insert description here...
+ *  ...
+ *
+ * Copyright (C) Jan. 2021: Georg Pohl, 70174 Stuttgart
+ */
+
+// no absolute path entries
+#include <iostream>
+#include "filename.h"
+
+/* EXTERNAL AND GLOBAL VARIABLES */
+
+/* CONSTANTS */
+
+/* STRUCTS, CLASSES, TYPEALIASE */
+
+/* STATIC VARIABLES */
+
+/* FUNCTIONS */
+
+/**
+ * Implementation notes: <functionName>
+ * ------------------------------------
+ * Here you can describe in detail how
+ * the implementation is done, what are
+ * the corner cases and what problems
+ * can arise due to wrong usage.
+ */
+
+int foo(int x) {
+  /* !TODO: gpohl 2021-01.01
+  ... not longer than 2000 lines. */
+
+  return EXIT_SUCCESS;
+}
+
+/* End of filename.cpp */
---
+++ HeaderVorlage.hpp
@@ -0,0 +1,32 @@
+/** 
+ * File: filename.hpp
+ * Author: Georg Pohl
+ * Date of creation: 1st of January 2021
+ * Version: 1.0
+ * Last change: 1st of January 2021
+ * -------------------------------------
+ * Implementation notes: <functionName>
+ * -------------------------------------
+ * Here you can describe in detail how
+ * the implementation is done, what are
+ * the corner cases and what problems
+ * can arise due to wrong usage.
+ *
+ * Copyright (C) Jan. 2021: Georg Pohl, 70174 Stuttgart
+ */
+#ifndef FILENAME_HPP_
+#define FILENAME_HPP_
+
+// no absolute path entries
+#include <iostream>
+
+
+// Typaliase (typedef)
+
+// Constants
+
+// Makros, Declarations, ...
+  int foo(int x);
+
+#endif */ FILENAME_HPP_ */
+/* End of filename.hpp */
